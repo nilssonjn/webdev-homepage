b260a6374daeef32a9edffc19e00e6cb3272de62
jnnilsson <jn.jensnilsson@gmail.com>
2024-08-30 14:29:36 +0200

    add fetch to display from json, some temporary css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const elRatingForm = document.getElementById("rating-form");
 
+const ul = document.querySelector('#ingredients-pizza');
+const url = 'data/recipes.json';
+
 function submitRatingForm(event) {
     let rating = document.getElementById("rating").value;
 
@@ -13,4 +16,28 @@ function submitRatingForm(event) {
 }
 
 elRatingForm.addEventListener("submit", submitRatingForm, false);
+
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data.recipes);
+        let recipes = data.recipes;
+
+        ul.innerHTML = '';
+
+        let pizzaRecipe = recipes.filter(function(recipe) {
+            return recipe.id === 1;
+        });
+
+        pizzaRecipe.forEach(function(recipe) {
+            recipe.ingredients.forEach(function(ingredient) {
+                let li = document.createElement('li');
+                li.textContent = ingredient;
+                ul.appendChild(li);
+            })
+        })
+        .catch(function(error) {
+            console.error(error);
+    });
+});
 

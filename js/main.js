812088a646cd3700cbd6ca5bfbdfc9cfdb07e678
jnnilsson <jn.jensnilsson@gmail.com>
2024-09-08 19:16:57 +0200

    add alert popup in rating form, with custom css

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 "use strict";
 const elRatingForm = document.getElementById("rating-form");
+const elAlertMessage = document.getElementById("alert-message");
+const elCloseButton = document.getElementById("close-alert");
+const elAlertPopup = document.getElementById("alert-popup");
 function submitRatingForm(event) {
     let rating = document.getElementById("rating").value;
 
     if (rating < 1 || rating > 5) {
-        alert("Rating must be between 1 and 5");
+        elAlertMessage.textContent = "Rating must be between 1 and 5";
     } else {
-        alert("Thank you for rating this recipe!");
+        elAlertMessage.textContent = "Thank you for rating this recipe!";
     }
     
+    elAlertPopup.style.display = "flex";
     event.preventDefault();
 }
+
+elCloseButton.addEventListener("click", function() {
+    elAlertPopup.style.display = "none";
+});
 
 elRatingForm.addEventListener("submit", submitRatingForm, false);
 

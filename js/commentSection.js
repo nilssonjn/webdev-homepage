00fa0f71ac2ba643723cf0c24a4c57286726a34c
jnnilsson <jn.jensnilsson@gmail.com>
2024-09-10 13:57:17 +0200

    add a name input, css for it, js regex

--- js/commentSection.js
+++ js/commentSection.js
@@ -1,21 +1,28 @@
 "use strict";
 
-const elCommentForm = document.getElementById("comment");
+const elNameInput = document.getElementById("name");
+const elCommentInput = document.getElementById("comment");
 const elCharCountDisplay = document.getElementById("comment-length");
 const allowedChars = /^[a-zA-Z0-9!?]*$/;
 
 function handleInput() {
 
-    if (!allowedChars.test(elCommentForm.value)) {
+    if (!allowedChars.test(elCommentInput.value, elNameInput.value)) {
         alert("Only alphanumeric characters and !? are allowed");
-        elCommentForm.value = elCommentForm.value.replace(/[^a-zA-Z0-9!?]/g, "");
+        elCommentInput.value = elCommentInput.value.replace(/[^a-zA-Z0-9!?]/g, "");
     }
 
-    const charLength = elCommentForm.value.length;
-    const maxLength = elCommentForm.maxLength;
+    if (!allowedChars.test(elNameInput.value)) {
+        alert("Only alphanumeric characters and !? are allowed");
+        elNameInput.value = elNameInput.value.replace(/[^a-zA-Z0-9!?]/g, "");
+    }
+
+    const charLength = elCommentInput.value.length;
+    const maxLength = elCommentInput.maxLength;
 
     elCharCountDisplay.textContent = `${charLength}/${maxLength} characters`;
 
 }
 
-elCommentForm.addEventListener("input", handleInput);
+elCommentInput.addEventListener("input", handleInput, false);
+elNameInput.addEventListener("input", handleInput, false);

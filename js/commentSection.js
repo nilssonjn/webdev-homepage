0621205ae1a9f357906070d769097f4026150015
jnnilsson <jn.jensnilsson@gmail.com>
2024-09-10 13:36:51 +0200

    add regex, alert for typing not allowed chars

--- js/commentSection.js
+++ js/commentSection.js
@@ -2,11 +2,20 @@
 
 const elCommentForm = document.getElementById("comment");
 const elCharCountDisplay = document.getElementById("comment-length");
+const allowedChars = /^[a-zA-Z0-9!?]*$/;
 
-elCommentForm.addEventListener("input", function() {
+function handleInput() {
+
+    if (!allowedChars.test(elCommentForm.value)) {
+        alert("Only alphanumeric characters and !? are allowed");
+        elCommentForm.value = elCommentForm.value.replace(/[^a-zA-Z0-9!?]/g, "");
+    }
+
     const charLength = elCommentForm.value.length;
     const maxLength = elCommentForm.maxLength;
 
     elCharCountDisplay.textContent = `${charLength}/${maxLength} characters`;
 
-});
+}
+
+elCommentForm.addEventListener("input", handleInput);
